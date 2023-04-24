e9369ca8cd32fd83fcb2426506c985b11bd32f5b
Isaac Alter <i_alter@yahoo.com>
2023-04-23 21:11:48 -0600

    Add feat: facade to handle service and get specific data needed as well as provide an additional layer of security

---
+++ spec/facades/map_facade_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe MapFacade do
+  before :each do
+    @test_facade = MapFacade.new("cinncinati,oh")
+  end
+
+  describe "instance methods" do
+    it "has a location" do
+      expect(@test_facade.location).to eq("cinncinati,oh")
+    end
+
+    it "can get coordinates from a location", :vcr do
+      expect(@test_facade.coordinates).to be_a(Hash)
+      expect(@test_facade.coordinates).to have_key(:lat)
+      expect(@test_facade.coordinates).to have_key(:lng)
+      expect(@test_facade.coordinates[:lat]).to eq(39.10713)
+      expect(@test_facade.coordinates[:lng]).to eq(-84.50413)
+    end
+  end
+end
