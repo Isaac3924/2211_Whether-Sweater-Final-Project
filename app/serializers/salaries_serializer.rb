92042b3bf8ef043b63425b879157a32bfbeceb8f
Isaac Alter <i_alter@yahoo.com>
2023-04-24 10:57:42 -0600

    8

--- app/serializers/salaries_serializer.rb
+++ app/serializers/salaries_serializer.rb
@@ -17,9 +17,10 @@ class SalariesSerializer
           },
           salaries: salaries.map do |salary|
            {
-            if salary[:job][:title].eq "Data Analyst" 
+            if salary[:job][:title] == "Data Analyst" || salary[:job][:title] == "Data Scientist" || salary[:job][:title] == "Mobile Developer" || salary[:job][:title] == "QA Engineer" || salary[:job][:title] == "Software Engineer" || salary[:job][:title] == "Systems Administratot" || salary[:job][:title] == "Web Developer"
               job_title: salary[:job][:title],
-              min: "$"salary[:salary_percentiles][:percentile_25].round.to_s,
+              min: "$"salary[:salary_percentiles][:percentile_25].sprintf('%.2f').gsub(/(\d)(?=(\d{3})+(?!\d))/, '\\1,'),
+              max: "$"salary[:salary_percentiles][:percentile_75].sprintf('%.2f').gsub(/(\d)(?=(\d{3})+(?!\d))/, '\\1,')
             end
            }
         }
